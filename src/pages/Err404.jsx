a6af325779759fad335730136c9648df5b5b387f
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-07-04 13:32:38 -0400

    routing practice

---
+++ src/pages/Err404.jsx
@@ -0,0 +1,13 @@
+import NavList from "../components/NavList";
+
+export default function Err404 () {
+    return (
+        <>
+        <h2>Err 404</h2>
+        <NavList />
+        <p>You have reached the end of the internet.</p>
+        <p>You have reached the end of the internet.</p>
+        <p>You have reached the end of the internet.</p>
+        </>
+    )
+}
